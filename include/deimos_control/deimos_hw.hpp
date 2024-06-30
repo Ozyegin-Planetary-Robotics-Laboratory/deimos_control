0da729563dac5723666bd49815093b85784e77dc
Toprak-Efe <efe.akkilic@ozu.edu.tr>
2024-06-30 23:18:48 +0000

    Added joint 5 and finished the hardware interface.

--- include/deimos_control/deimos_hw.hpp
+++ include/deimos_control/deimos_hw.hpp
@@ -4,43 +4,80 @@
 #include <map>
 #include <array>
 #include <vector>
-#include <tmotor/MotorFeedback.h>
-#include <tmotor/MotorCommand.h>
+#include <tmotor.hpp>
+#include "../dynamixel_sdk/dynamixel_sdk.h"
+#include <ros/ros.h>
 #include <hardware_interface/robot_hw.h>
 #include <hardware_interface/posvelacc_command_interface.h>
+#include <hardware_interface/posvel_command_interface.h>
 #include <hardware_interface/joint_state_interface.h>
 #include <controller_manager/controller_manager.h>
 
-/**
- * @brief The RoboticArm class
- * 
- * This class is the main class of the robotic arm. It is responsible for interfacing with the TMotor AK60 library and the ROS control system. 
- * It also handles the transmission interfaces for mechanical reduction.
- */
-class RoboticArm : public hardware_interface::RobotHW {
-public:
-  RoboticArm();
-  void write();
+#define TMOTORS_FIRST_ID 11 
+#define CAN_NETWORK "can0"
 
-private:
-  /* Robot configuration */
-  static const size_t m_number_of_joints;
-  static const std::vector<double> m_joint_reductions;
-  static const std::vector<std::string> m_joint_names;
-  ros::NodeHandle m_nh;
-  std::vector<ros::Publisher> m_pubs;
-  std::vector<ros::Subscriber> m_subs;
-  hardware_interface::JointStateInterface m_jnt_state_interface;
-  hardware_interface::PosVelAccJointInterface m_posvelacc_joint_interface;
-  bool m_shutdown;
-  std::vector<double> m_arr_curr_pos;
-  std::vector<double> m_arr_curr_vel;
-  std::vector<double> m_arr_curr_eff;
-  std::vector<double> m_arr_cmd_pos;
-  std::vector<double> m_arr_cmd_vel;
-  std::vector<double> m_arr_cmd_acc;
+#define PROTOCOL_VERSION                1.0
+#define DXL_ID                          1
+#define BAUDRATE                        1000000
+#define DEVICENAME                      "/dev/ttyUSB0"
 
-  void motorFeedbackCallback(const tmotor::MotorFeedback::ConstPtr& msg, int idx);
-};
+#define ADDR_MX_TORQUE_ENABLE           24
+#define ADDR_MX_GOAL_POSITION           30
+#define ADDR_MX_MOVING_SPEED            32
+#define ADDR_MX_PRESENT_POSITION        36
+#define ADDR_MX_MOVING                  46
+
+// Data Byte Length
+#define LEN_MX_GOAL_POSITION            2
+#define LEN_MX_PRESENT_POSITION         2
+#define LEN_MX_MOVING                   1
+
+#define TORQUE_ENABLE                   1
+#define TORQUE_DISABLE                  0
+#define DXL_MOVING_STATUS_THRESHOLD     10
+
+namespace deimos_control
+{
+  /**
+   * @brief The RoboticArm class
+   * 
+   * This class is the main class of the robotic arm. It is responsible for interfacing with the TMotor AK60 library and the ROS control system. 
+   * It also handles the transmission interfaces for mechanical reduction.
+   */
+  class RoboticArm : public hardware_interface::RobotHW
+  {
+  public:
+    RoboticArm();
+    void write();
+    void read();
+
+  private:
+    /* Robot configuration */
+    enum MotorType
+    {
+      DYNAMIXEL,
+      AK60
+    };
+
+    static const size_t m_number_of_joints;
+    static const std::vector<double> m_joint_reductions;
+    static const std::vector<std::string> m_joint_names;
+    std::vector<TMotor::AKManager> m_ak_managers;
+    dynamixel::PortHandler *m_port_handler;
+    dynamixel::PacketHandler *m_packet_handler;
+    ros::NodeHandle m_nh;
+    hardware_interface::JointStateInterface m_jnt_state_interface;
+    hardware_interface::PosVelJointInterface m_posvel_joint_interface;
+    hardware_interface::PosVelAccJointInterface m_posvelacc_joint_interface;
+    std::vector<double> m_arr_curr_pos;
+    std::vector<double> m_arr_curr_vel;
+    std::vector<double> m_arr_curr_eff;
+    std::vector<double> m_arr_cmd_pos;
+    std::vector<double> m_arr_cmd_vel;
+    std::vector<double> m_arr_cmd_acc;
+  }; // class RoboticArm
+} // namespace deimos_control
+
+
 
 #endif // DEIMOS_HW_HPP
--- src/deimos_hw.cpp
+++ src/deimos_hw.cpp
@@ -1,68 +1,121 @@
+#include "../include/dynamixel_sdk/dynamixel_sdk.h"
 #include "../include/deimos_control/deimos_hw.hpp"
 
-const size_t RoboticArm::m_number_of_joints(4);
-const std::vector<double> RoboticArm::m_joint_reductions = {29, 29, 20, 10};
-const std::vector<std::string> RoboticArm::m_joint_names = {"joint1", "joint2", "joint3", "joint4"};
+const size_t deimos_control::RoboticArm::m_number_of_joints(4);
+const std::vector<double> deimos_control::RoboticArm::m_joint_reductions = {29, 44, 20, 10};
+const std::vector<std::string> deimos_control::RoboticArm::m_joint_names = {"joint1", "joint2", "joint3", "joint4"};
 
-RoboticArm::RoboticArm():
-  m_shutdown(false),
-  m_nh("/")
+deimos_control::RoboticArm::RoboticArm():
+  m_port_handler(dynamixel::PortHandler::getPortHandler(DEVICENAME)),
+  m_packet_handler(dynamixel::PacketHandler::getPacketHandler(PROTOCOL_VERSION)),
+  m_nh("~")
 { 
-  // Initialize member arrays.
-  m_arr_cmd_acc.resize(m_number_of_joints, 0.0);
-  m_arr_cmd_pos.resize(m_number_of_joints, 0.0);
-  m_arr_cmd_vel.resize(m_number_of_joints, 0.0);
-  m_arr_curr_eff.resize(m_number_of_joints, 0.0);
-  m_arr_curr_pos.resize(m_number_of_joints, 0.0);
-  m_arr_curr_vel.resize(m_number_of_joints, 0.0);
-  for (int i = 0; i < m_number_of_joints; i++) {
+  // Initialize TMotor AK60s
+  m_arr_cmd_acc.resize(m_number_of_joints+1, 0.0);
+  m_arr_cmd_pos.resize(m_number_of_joints+1, 0.0);
+  m_arr_cmd_vel.resize(m_number_of_joints+1, 0.0);
+  m_arr_curr_eff.resize(m_number_of_joints+1, 0.0);
+  m_arr_curr_pos.resize(m_number_of_joints+1, 0.0);
+  m_arr_curr_vel.resize(m_number_of_joints+1, 0.0);
+  m_ak_managers.reserve(m_number_of_joints+1);
+  for (int i = 0; i < m_number_of_joints; i++)
+  {
     m_arr_curr_pos[i] = 0.0;
     m_arr_curr_vel[i] = 0.0;
     m_arr_curr_eff[i] = 0.0;
     m_arr_cmd_pos[i] = 0.0;
     m_arr_cmd_vel[i] = 0.0;
     m_arr_cmd_acc[i] = 0.0;
-    std::string feedback_topic = "actuator" + std::to_string(i+1) + "/motor_feedback";
-    std::string command_topic = "actuator" + std::to_string(i+1) + "/motor_command";
-    m_pubs.emplace_back(m_nh.advertise<tmotor::MotorCommand>(command_topic, 1));
-    m_subs.emplace_back(m_nh.subscribe<tmotor::MotorFeedback>(feedback_topic, 1, boost::bind(&RoboticArm::motorFeedbackCallback, this, _1, i)));
     hardware_interface::PosVelAccJointHandle posvelacc_handle(m_jnt_state_interface.getHandle(m_joint_names[i]), &m_arr_cmd_pos[i], &m_arr_cmd_vel[i], &m_arr_cmd_acc[i]);
     hardware_interface::JointStateHandle state_handle(m_joint_names[i], &m_arr_curr_pos[i], &m_arr_curr_vel[i], &m_arr_curr_eff[i]);
     m_posvelacc_joint_interface.registerHandle(posvelacc_handle);
     m_jnt_state_interface.registerHandle(state_handle);
+    m_ak_managers.emplace_back(i + TMOTORS_FIRST_ID);
+    m_ak_managers[i].connect(CAN_NETWORK);
   }
+
+  // Initialize Dynamixel motors
+  hardware_interface::PosVelJointHandle posvel_handle(m_jnt_state_interface.getHandle("joint5"), &m_arr_cmd_pos[4], &m_arr_cmd_vel[4]);
+  hardware_interface::JointStateHandle state_handle("joint5", &m_arr_curr_pos[4], &m_arr_curr_vel[4], &m_arr_curr_eff[4]);
+  m_posvel_joint_interface.registerHandle(posvel_handle);
+  m_jnt_state_interface.registerHandle(state_handle);
+
+  if (!m_port_handler->openPort())
+  {
+    ROS_ERROR("Failed to open the port!");
+  }
+  if (!m_port_handler->setBaudRate(1000000))
+  {
+    ROS_ERROR("Failed to change the baudrate!");
+  }
+
   registerInterface(&m_posvelacc_joint_interface);
+  registerInterface(&m_posvel_joint_interface);
   registerInterface(&m_jnt_state_interface);
 }
 
-void RoboticArm::write() {
-  for (size_t i = 0; i < m_number_of_joints; i++) {
-    tmotor::MotorCommand msg;
-    msg.position = m_arr_cmd_pos[i];
-    msg.velocity = m_arr_cmd_vel[i];
-    msg.acceleration = m_arr_cmd_acc[i];
-    m_pubs[i].publish(msg);
+void deimos_control::RoboticArm::write()
+{
+  for (int i = 0; i < m_number_of_joints; i++)
+  {
+    m_ak_managers[i].sendPositionVelocityAcceleration(m_arr_cmd_pos[i], m_arr_cmd_vel[i], m_arr_cmd_acc[i]);
+  }
+  uint8_t dxl_write_error = 0;
+  int dxl_write_result = m_packet_handler->write2ByteTxRx(m_port_handler, DXL_ID, ADDR_MX_GOAL_POSITION,  static_cast <uint16_t> (m_arr_cmd_pos[4]/360.0)*4096, &dxl_write_error);
+  if (dxl_write_result != COMM_SUCCESS)
+  {
+    ROS_ERROR("%s", m_packet_handler->getTxRxResult(dxl_write_result));
+  }
+  else if (dxl_write_error != 0)
+  {
+    ROS_ERROR("%s", m_packet_handler->getRxPacketError(dxl_write_error));
   }
 }
 
-void RoboticArm::motorFeedbackCallback(const tmotor::MotorFeedback::ConstPtr& msg, int idx) {
-  m_arr_curr_pos[idx] = msg->position;
-  m_arr_curr_vel[idx] = msg->velocity;
-  m_arr_curr_eff[idx] = msg->current;
+void deimos_control::RoboticArm::read()
+{
+  for (int i = 0; i < m_number_of_joints; i++)
+  {
+    m_arr_curr_pos[i] = m_ak_managers[i].getPosition();
+    m_arr_curr_vel[i] = m_ak_managers[i].getVelocity();
+    m_arr_curr_eff[i] = m_ak_managers[i].getCurrent();
+  }
+  uint8_t dxl_read_error = 0;
+  uint16_t dxl_present_position = 0;
+  int dxl_comm_result = m_packet_handler->read2ByteTxRx(m_port_handler, DXL_ID, ADDR_MX_PRESENT_POSITION, &dxl_present_position, &dxl_read_error);
+  if (dxl_comm_result != COMM_SUCCESS)
+  {
+    ROS_ERROR("%s", m_packet_handler->getTxRxResult(dxl_comm_result));
+  }
+  else if (dxl_read_error != 0)
+  {
+    ROS_ERROR("%s", m_packet_handler->getRxPacketError(dxl_read_error));
+  }
+  return;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   ros::init(argc, argv, "deimos_robot");
-  RoboticArm deimos;
+  
+  deimos_control::RoboticArm deimos;
   controller_manager::ControllerManager cm(&deimos);
-  ros::Time prev_time = ros::Time(0);
-  ros::Rate rate(30.0); // 10 Hz rate
-  while (ros::ok()) {
-    const ros::Time time = ros::Time::now();
-    ros::spinOnce();
-    cm.update(time, time - prev_time);
-    prev_time = time;
+  
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+  ros::Time prev_time = ros::Time::now();
+  ros::Rate rate(10.0);
+
+  while (ros::ok())
+  {
+    const ros::Time     time   = ros::Time::now();
+    const ros::Duration period = time - prev_time;
+
+    deimos.read();
+    cm.update(time, period);
     deimos.write();
+
     rate.sleep();
   }
 }
