db6a7f2c833f1e2c77ae3f661148a15935dbbb88
Toprak <efe.akkilic@ozu.edu.tr>
2024-08-03 15:08:34 +0300

    Final pre-test procedures.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,40 @@
 #include <tmotor.hpp>
 
 static const float MOTOR_MAX_VELOCITY = 5.0f;
-std::vector<float> velocity_commands(4, 0.0);
+std::vector<float> velocity_commands(4, 0.0f);
+
+void joyCallback(const sensor_msgs::Joy::ConstPtr &msg);
+void initializeMotors(std::vector<TMotor::AKManager> &motors);
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "joystick");
+    ros::NodeHandle nh;
+    ros::Subscriber joy_sub = nh.subscribe<sensor_msgs::Joy> ("joy", 10, &joyCallback);  
+
+    std::vector <TMotor::AKManager> motors;
+    try
+    {
+        initializeMotors(motors);
+    }
+    catch(const std::exception& e)
+    {
+        ROS_ERROR("Failed to initialize motors: %s", e.what());
+        return 1;
+    }
+
+    ros::Rate command_frequency(10.0);
+    while (ros::ok())
+    {
+        command_frequency.sleep();
+        ros::spinOnce();
+        for (size_t i = 0; i < 4; ++i)
+        {
+            motors[i].sendVelocity(velocity_commands[i]);
+            velocity_commands[i] *= 0.8;
+        }
+    }
+}
 
 void joyCallback(const sensor_msgs::Joy::ConstPtr &msg)
 {
@@ -29,60 +62,38 @@ void joyCallback(const sensor_msgs::Joy::ConstPtr &msg)
 
     if (!toggle_eof)
     {
-
-        velocity_commands[0] = msg->axes[2];
-        velocity_commands[1] = msg->axes[1];
-        velocity_commands[2] = msg->axes[0];
-        velocity_commands[3] = msg->axes[5];
+        velocity_commands[0] = std::max(-MOTOR_MAX_VELOCITY, std::min(MOTOR_MAX_VELOCITY, msg->axes[2]*MOTOR_MAX_VELOCITY));
+        velocity_commands[1] = std::max(-MOTOR_MAX_VELOCITY, std::min(MOTOR_MAX_VELOCITY, msg->axes[1]*MOTOR_MAX_VELOCITY));
+        velocity_commands[2] = std::max(-MOTOR_MAX_VELOCITY, std::min(MOTOR_MAX_VELOCITY, msg->axes[0]*MOTOR_MAX_VELOCITY));
+        velocity_commands[3] = std::max(-MOTOR_MAX_VELOCITY, std::min(MOTOR_MAX_VELOCITY, msg->axes[5]*MOTOR_MAX_VELOCITY));
     }
     else
     {
-        velocity_commands[0] = 0.0;
-        velocity_commands[1] = 0.0;
-        velocity_commands[2] = 0.0;
-        velocity_commands[3] = 0.0;
-        // to do - dynamixel and eof
+        velocity_commands[0] = 0.0f;
+        velocity_commands[1] = 0.0f;
+        velocity_commands[2] = 0.0f;
+        velocity_commands[3] = 0.0f;
     }
 }
 
-int main(int argc, char** argv)
+void initializeMotors(std::vector<TMotor::AKManager> &motors)
 {
-    ros::init(argc, argv, "joystick");
-    ros::NodeHandle nh;
-
     std::vector<int> motor_ids;
     ros::param::get("motor_ids", motor_ids);
     if (motor_ids.size() != 4)
     {
-        ROS_ERROR("Motor ids must be a list of 4 integers");
-        return 1;
+        throw std::runtime_error("Invalid motor ids");
     }
     std::string can_interface;
     ros::param::get("can_interface", can_interface);
     if (can_interface != "can0" && can_interface != "vcan0")
     {
-        ROS_ERROR("CAN interface must be either can0 or can1");
-        return 1;
+        throw std::runtime_error("Invalid CAN interface.");
     }
-    std::vector <TMotor::AKManager> motors;
-
     motors.reserve(4);
     for (std::vector<int>::const_iterator id = motor_ids.begin(); id != motor_ids.end(); ++id)
     {
         motors.emplace_back(*id);
         motors.back().connect(can_interface.c_str());
-    }
-
-    ros::Subscriber joy_sub = nh.subscribe<sensor_msgs::Joy> ("joy", 10, &joyCallback);    
-    ros::Rate command_frequency(10.0);
-    while (ros::ok())
-    {
-        command_frequency.sleep();
-        ros::spinOnce();
-        for (size_t i = 0; i < 4; ++i)
-        {
-            motors[i].sendVelocity(velocity_commands[i]);
-            velocity_commands[i] *= 0.95;
-        }
     }
 }
